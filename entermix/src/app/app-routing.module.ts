68ae0e8111276a873cba4ee2540af3bef5015571
Guilherme Ramos Lourenco <guiramoslou@gmail.com>
2020-07-04 13:07:44 -0300

    adicionado rotas, html/css para aboutUs e contato

--- entermix/src/app/app-routing.module.ts
+++ entermix/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuPageLayoutComponent } from 'src/app/mixcomp/menu-page-layout/menu-page-layout.component'
 import { MixcompComponent } from './mixcomp/mixcomp.component';
+import { componentFactoryName } from '@angular/compiler';
+import { AboutUsComponent } from './about-us/about-us.component';
+import {ContatoComponent} from './contato/contato/contato.component'
 
 
 const routes: Routes = [
@@ -12,7 +15,15 @@ const routes: Routes = [
     {
       path:'',
       component: MixcompComponent,
-    }
+    },
+    {
+      path:'aboutUs',
+      component: AboutUsComponent,
+    },
+    {
+      path:'contato',
+      component: ContatoComponent,
+    },
   ]
 }
 
