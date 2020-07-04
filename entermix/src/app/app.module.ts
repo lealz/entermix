7db7db2656c390ae44f2832befcae2b352a4bcc0
lealz <matheus.mlealzzera@gmail.com>
2020-07-04 13:29:11 -0300

    arrumando conflito de merge

--- entermix/src/app/app.module.ts
+++ entermix/src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 
-=======
 import { FormsModule } from '@angular/forms'
->>>>>>> 68ae0e8111276a873cba4ee2540af3bef5015571
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { MatCardModule } from '@angular/material/card';
 import { MixcompComponent } from './mixcomp/mixcomp.component';
-import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MixCardComponent } from './mixcomp/mix-card/mix-card.component';
 import { MenuPageLayoutComponent } from './mixcomp/menu-page-layout/menu-page-layout.component';
@@ -39,7 +34,6 @@ import {MatMenuModule} from '@angular/material/menu';
     BrowserAnimationsModule,
     MatCardModule,
     FormsModule,
-    MatCardModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
