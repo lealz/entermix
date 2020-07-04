9c24676154abef62120b8e0e1c60361e6397b84c
Guilherme Ramos Lourenco <guiramoslou@gmail.com>
2020-07-04 12:12:18 -0300

    resolvido alguns problemas com merge, style css finalmente funciona!

--- entermix/src/app/app.module.ts
+++ entermix/src/app/app.module.ts
@@ -1,20 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { AboutUsComponent } from './about-us/about-us.component';
-import {MatCardModule} from '@angular/material/card';
-import { FormsModule } from '@angular/forms';
-
-=======
+import { MatCardModule } from '@angular/material/card';
 import { MixcompComponent } from './mixcomp/mixcomp.component';
-<<<<<<< HEAD
->>>>>>> ba2dcb54f75c8266aed96067e09bc912199fdd66
-=======
 import { MenuPageLayoutComponent } from './mixcomp/menu-page-layout/menu-page-layout.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -22,30 +14,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
->>>>>>> 0c2bf1134cc543384f1de69100d2baa3a981ceb4
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    AboutUsComponent
-=======
-    MixcompComponent
->>>>>>> ba2dcb54f75c8266aed96067e09bc912199fdd66
-=======
+    AboutUsComponent,
     MixcompComponent,
     MenuPageLayoutComponent
->>>>>>> 0c2bf1134cc543384f1de69100d2baa3a981ceb4
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
     MatCardModule,
-    FormsModule
-=======
     FormsModule,
     LayoutModule,
     MatToolbarModule,
@@ -53,7 +34,6 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule
->>>>>>> 0c2bf1134cc543384f1de69100d2baa3a981ceb4
   ],
   providers: [],
   bootstrap: [AppComponent]
