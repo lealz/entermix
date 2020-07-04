aa8b8572fdde34dc144c22a70cbdb0e7862b9d25
willianmelo98 <willianmelob@gmail.com>
2020-07-04 16:31:51 -0300

    final WIllian

--- entermix/src/app/passaTempos/passa-tempos/passa-tempos.component.ts
+++ entermix/src/app/passaTempos/passa-tempos/passa-tempos.component.ts
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PassaTemposComponent implements OnInit {
 
-  escapeDaPrisão:string='https://poki.com.br/g/escapada-final-da-pris%C3%A3o?campaign=1784657719&adgroup=103758857471&target=&location=1001773&creative=434078648624&placement=rachacuca.com.br&gclid=EAIaIQobChMI9cfphZK06gIVwblRCh0O0AdZEAEYASACEgKbnfD_BwE';
   einsten:string='https://rachacuca.com.br/teste-de-einstein/';
   rachaCuca:string='https://rachacuca.com.br/';
   imageEinsten:string='https://rachacuca.com.br/media/cache/70/78/7078d20d3626848597c62a122e35c56d.png';
   rachaCucaImage:string='https://rachacuca.com.br/static/imagens/logo-racha-cuca.png';
+  quaseNadaImg:string='https://rachacuca.com.br/media/cache/64/82/6482869010f982b9fc9ab79e8067766d.png';
+  raboxImg:string='https://rachacuca.com.br/media/cache/f1/ee/f1ee65e59b3db0c1dc43c66eb4617907.png';
+  ultimoImg:string='https://rachacuca.com.br/static/jogos/imagens/jogo-do-cupido_180x111.png';
 
   constructor() { }
 
